89545ce779f1ce4b17b2bb46cfd61a9a2ddec7f6
melvlaal <melvlaal@gmail.com>
2025-02-12 17:05:33 +0200

    init commit

---
+++ src/test/java/com/example/picheTest/PicheTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picheTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicheTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
