cba1ede1a77563a7a8b51e14d9e3ae7f17075a50
melvlaal <melvlaal@gmail.com>
2025-02-12 17:05:33 +0200

    init commit

---
+++ src/main/java/com/example/picheTest/PicheTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.picheTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicheTestApplication.class, args);
+	}
+
+}
